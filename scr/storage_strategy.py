f56b7528ba589e6fa9d5da1ded48a726a6a3217b
GregorJM <49516496+gregkalle@users.noreply.github.com>
2024-10-14 13:02:17 +0200

    sqlite and storage

--- scr/storage_strategy.py
+++ scr/storage_strategy.py
@@ -4,12 +4,15 @@ class StorageStrategy(ABC):
     """abstract class to set the methods for the storag strategy"""
     @abstractmethod
     def save(self, habit):
+        """save the habit"""
         pass
 
     @abstractmethod
     def load(self, habit_id):
+        """load the habit"""
         pass
 
     @abstractmethod
     def delete(self, habit_id):
+        """delete the habit"""
         pass
