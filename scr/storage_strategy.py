d8bc677aea9ed116fab3f5f817d95652b5168850
GregorJM <49516496+gregkalle@users.noreply.github.com>
2024-10-14 12:23:34 +0200

    documentation storag strategy

--- scr/storage_strategy.py
+++ scr/storage_strategy.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 
 class StorageStrategy(ABC):
-
+    """abstract class to set the methods for the storag strategy"""
     @abstractmethod
-    def save(self, habit_complition_manager):
+    def save(self, habit):
         pass
 
     @abstractmethod
-    def load(self, manager_id):
+    def load(self, habit_id):
+        pass
+
+    @abstractmethod
+    def delete(self, habit_id):
         pass
