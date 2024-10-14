e852942c11a5b12a582810bfdcb724cb68fd76cd
GregorJM <49516496+gregkalle@users.noreply.github.com>
2024-10-14 13:07:50 +0200

    ananlytics first

---
+++ scr/analytics.py
@@ -0,0 +1,28 @@
+from habit import Habit
+
+class Analytics:
+    """
+    
+    """
+
+    def __init__(self):
+        """
+
+        """
+        self.all_habits = Habit.load_all()
+    
+    @classmethod
+    def current_tracked_habit(cls, habit_list, habit_id):
+        pass
+
+    @classmethod
+    def habit_with_frequency(cls, habit_list, frequency):
+        pass
+
+    @classmethod
+    def get_longest_streaks(cls, habit_list):
+        pass
+    
+    @classmethod
+    def most_struggled_habit(cls, habit_list):
+        pass
