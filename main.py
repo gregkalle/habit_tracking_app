c4ab4e6a974b5106d04a7e6b98ad0c0665af4184
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-10-21 10:39:37 +0200

    delete unnecessary methodes of the App class in main.py

--- main.py
+++ main.py
@@ -34,67 +34,7 @@ class App(tk.Tk):
         title = ttk.Label(top_frame, text="Habit Tracking App")
         title.pack()
         return top_frame 
-       
-
-    def get_bottom_frame(self):
-        bottom_frame = ttk.Frame(self)
-        
-        
-        for button in self.BUTTONS.keys():
-            self.get_button(bottom_frame,button,self.BUTTONS[button]).pack(side="left")
-        
-        self.get_menu_button(frame=bottom_frame,title="select periodicity",item_selection=App.SELECTABLE_FREQUENCIES,
-                             selected_item=self.selected_frequency).pack(side="left")
-        self.get_menu_button(bottom_frame,"sort by value",App.SELECTABLE_VALUES,self.selected_value).pack(side="left")
-        
-        return bottom_frame
-
-    def get_menu_button(self, frame, title, item_selection, selected_item):
-
-        menu_button = ttk.Menubutton(frame, text=title)
-        menu = tk.Menu(menu_button)
-
-        for select in item_selection:
-            menu.add_radiobutton(
-                label=select,
-                value=select.upper(),
-                variable=selected_item
-            )
-        menu_button["menu"] = menu
-        return menu_button
-
-
-    def get_button(self, frame, text, function):
-        return ttk.Button(frame, text=text, command=function, padding=10)
     
-    def get_radio_buttons(self, frame, integer_list):
-        return [ttk.Radiobutton(frame, value=integer, variable=self.selected_habit_id) for integer in integer_list]
-    
-    def frequency_selected(self, *args):
-        frequency_name = self.selected_frequency.get()
-        if frequency_name in App.USABLE_FREQUENCIES.keys():
-            habit_list = Analytics.get_habits_with_frequency(self.analytics.all_habits, App.USABLE_FREQUENCIES[frequency_name])
-        else:
-            habit_list = self.analytics.all_habits
-
-        self.reload_center_frame(Analytics.HABIT_LIST_TITLES,habit_list)
-            
-
-    def value_selected(self, *args):
-        self.reload_center_frame(Analytics.HABIT_LIST_TITLES, self.analytics.all_habits)
-
-    def click_check_date(self):
-        pass
-
-    def click_new_habit(self):
-        pass
-
-    def click_change_habit(self):
-        pass
-
-    def click_delete_habit(self):
-        pass
-
     def reload_center_frame(self, column_names, habit_list):
         self.center_frame.destroy()
         self.center_frame = CenterFrame(self, column_names, habit_list)
