5137602ad5d069eb05ce973c81f565a839dcd65a
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-10-28 10:24:03 +0100

    migrate PopCalendar and DatePicker to entry_window.py modul and add docstrings

--- scr/entry_window.py
+++ scr/entry_window.py
@@ -1,9 +1,20 @@
+"""
+NAME
+    entry_window - create a pop-up window of the habit tracking app
+
+CLASSES
+    pop_up_wondow.PopUpWindow
+        EntryPopUp
+        DatePicker
+        PopUpCalendar
+"""
 import tkinter as tk
 from tkinter import ttk
 import tkinter.messagebox as mb
+from datetime import date, timedelta
+from tkcalendar import Calendar
 from scr.pop_up_window import PopUpWindow
 from scr.analytics import Analytics
-
 
 class EntryPopUp(PopUpWindow):
 
@@ -141,3 +152,110 @@ class EntryPopUp(PopUpWindow):
             self.main_window.analytics.load_habits()
             self.main_window.reload_center_frame(self.main_window.analytics.all_habits)
         self.destroy()
+
+
+class DatePicker(PopUpWindow):
+
+    """
+    A pop-up window for selecting dates using a calendar widget.
+    """
+
+    def __init__(self, main_window):
+        """
+        Initialize the date picker window.
+        
+        Args:
+            main_window (Tk): The main application window.
+        """
+        super().__init__(main_window=main_window)
+
+        self.title("select date")
+        self.calendar = Calendar(master=self, selectmode="day")
+        self.calendar.pack()
+        self.pack_buttons()
+
+    def pack_buttons(self):
+        """
+        Create and pack the buttons for the date picker window.
+        """
+        button_frame = ttk.Frame(self)
+        ttk.Button(button_frame, text="today", command=self.click_today,
+               padding=10).pack(side="left")
+        ttk.Button(button_frame, text="select date", command=self.click_date,
+               padding=10).pack(side="left")
+        ttk.Button(button_frame, text="Cancel", command=self.destroy,
+               padding=10).pack(side="left")
+        button_frame.pack(side="bottom")
+
+    def click_today(self):
+        """
+        Mark the habit as completed for today and update the main window.
+        """
+        habit = Analytics.get_marked_completed(\
+            habit_id=self.main_window.center_frame.selected_habit_id.get())
+        habit.save()
+        self.main_window.analytics.load_habits()
+        self.main_window.reload_center_frame(self.main_window.analytics.all_habits)
+        self.destroy()
+
+    def click_date(self):
+        """
+        Mark the habit as completed for the selected date and update the main window.
+        """
+        date_str = self.calendar.get_date()
+        month, day, year = [int(date_) for date_ in date_str.split("/")]
+        year = 2000 + year
+        habit = Analytics.get_marked_completed(\
+            habit_id=self.main_window.center_frame.selected_habit_id.get(),
+            date=date(year=year,month=month,day=day)
+            )
+        habit.save()
+        self.main_window.analytics.load_habits()
+        self.main_window.reload_center_frame(self.main_window.analytics.all_habits)
+        self.destroy()
+
+
+class PopUpCalendar(PopUpWindow):
+    """
+        Initialize a pop-up calendar window that displays a calendar
+        with completed dates highlighted.
+        
+        Args:
+            main_window (Tk): The main application window.
+            completed_dates (list): A list of dates that have been completed.
+            frequency (int): The frequency of the habit.
+        """
+    def __init__(self, main_window, completed_dates=None, frequency=1):
+        super().__init__(main_window)
+
+        self.title("completed dates")
+        self.calendar = Calendar(master=self, selectmode="day")
+        self.calendar.pack()
+        self.completed_dates = completed_dates
+        if completed_dates is None:
+            self.completed_dates=[]
+        self.frequency=frequency
+        self.check_dates(completed_dates=self.completed_dates, frequency=self.frequency)
+
+    def check_dates(self,completed_dates,frequency):
+        """
+        Highlight the completed dates on the calendar.
+        
+        Args:
+            completed_dates (list): A list of dates that have been completed.
+            frequency (int): The frequency of the habit.
+        """
+        for completed in completed_dates:
+            for i in range(frequency):
+
+                if i==0:
+                    #set backgroundcolor of the first day of a period to green
+                    self.calendar.calevent_create(date=completed+timedelta(days=i),
+                                              text='Hello World', tags= "Day one")
+                    self.calendar.tag_config("Day one", background='green', foreground='white')
+                else:
+                    #set backgroundcolor of the other days of a period to lightgreen
+                    self.calendar.calevent_create(date=completed+timedelta(days=i),
+                                              text='Hello World', tags= "The other days")
+                    self.calendar.tag_config("The other days", background="#49cc6c",
+                                             foreground='white')
--- scr/pop_up_window.py
+++ scr/pop_up_window.py
@@ -1,8 +1,15 @@
+"""
+NAME
+    pop_up_window - contains the parent for all kind of pop-up windows of the habit tracking app
+
+DESCRIPTION
+    Override the destroy method of tkinter.Tk
+
+CLASSES
+    tkinter.Tk
+        PopUpWindow
+"""
 from tkinter import Tk
-from tkinter.ttk import Frame, Button
-from datetime import date, timedelta
-from tkcalendar import Calendar
-from scr.analytics import Analytics
 
 class PopUpWindow(Tk):
 
@@ -28,86 +35,3 @@ class PopUpWindow(Tk):
         except AttributeError as exc:
             raise AttributeError("Mai window has no attribut remove_child_window") from exc
         return super().destroy()
-
-
-class DatePicker(PopUpWindow):
-
-    """
-    A pop-up window for selecting dates using a calendar widget.
-    """
-
-    def __init__(self, main_window):
-        """
-        Initialize the date picker window.
-        
-        Args:
-            main_window (Tk): The main application window.
-        """
-        super().__init__(main_window=main_window)
-
-        self.title("select date")
-        self.calendar = Calendar(master=self, selectmode="day")
-        self.calendar.pack()
-        self.pack_buttons()
-
-    def pack_buttons(self):
-        """
-        Create and pack the buttons for the date picker window.
-        """
-        button_frame = Frame(self)
-        Button(button_frame, text="today", command=self.click_today,
-               padding=10).pack(side="left")
-        Button(button_frame, text="select date", command=self.click_date,
-               padding=10).pack(side="left")
-        Button(button_frame, text="Cancel", command=self.destroy,
-               padding=10).pack(side="left")
-        button_frame.pack(side="bottom")
-
-    def click_today(self):
-        """
-        Mark the habit as completed for today and update the main window.
-        """
-        habit = Analytics.get_marked_completed(\
-            habit_id=self.main_window.center_frame.selected_habit_id.get())
-        habit.save()
-        self.main_window.analytics.load_habits()
-        self.main_window.reload_center_frame(self.main_window.analytics.all_habits)
-        self.destroy()
-
-    def click_date(self):
-        """
-        Mark the habit as completed for the selected date and update the main window.
-        """
-        date_str = self.calendar.get_date()
-        month, day, year = [int(date_) for date_ in date_str.split("/")]
-        year = 2000 + year
-        habit = Analytics.get_marked_completed(\
-            habit_id=self.main_window.center_frame.selected_habit_id.get(),
-            date=date(year=year,month=month,day=day)
-            )
-        habit.save()
-        self.main_window.analytics.load_habits()
-        self.main_window.reload_center_frame(self.main_window.analytics.all_habits)
-        self.destroy()
-
-
-class PopUpCalendar(PopUpWindow):
-
-    def __init__(self, main_window, completed_dates=None, frequency=1):
-        super().__init__(main_window)
-
-        self.title("completed dates")
-        self.calendar = Calendar(master=self, selectmode="day")
-        self.calendar.pack()
-        self.completed_dates = completed_dates
-        if completed_dates is None:
-            self.completed_dates=[]
-        self.frequency=frequency
-        self.check_dates(completed_dates=self.completed_dates, frequency=self.frequency)
-
-    def check_dates(self,completed_dates,frequency):
-        for completed in completed_dates:
-            for i in range(frequency):
-                self.calendar.calevent_create(date=completed+timedelta(days=i),
-                                              text='Hello World', tags= "Message")
-                self.calendar.tag_config("Message", background='green', foreground='white')
