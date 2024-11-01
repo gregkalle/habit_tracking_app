576e181a7fb9f21a76d391d8536178d6f41ffbab
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-11-01 11:08:10 +0100

    minor changes test_analytics

--- test/test_analytics.py
+++ test/test_analytics.py
@@ -3,6 +3,7 @@ from scr.analytics import Analytics
 from scr.habit import Habit
 from scr.sqlite_storage import SQLiteStorage
 
+#set database to test database
 Habit.DEFAULT_STORAGE_STRATEGY = SQLiteStorage("test/test_data.db")
 
 test_habit1=Habit(name="Go for a walk",description="Walking at least 1km a day.",habit_id=1)
@@ -24,8 +25,9 @@ def test_get_habit_ids(habit):
     for j,habit_id in enumerate(id_list):
         assert j+1== habit_id
 
-def test_create_new_habit():
-    habit1 = Analytics.create_new_habit(habit_name="Name",habit_description="Description",frequency=7)
-    habit2 = Analytics.create_new_habit(habit_name=None,habit_description=None,frequency=None)
-    assert habit1 == Habit(name="Name",description="Description",frequency=7) and habit1.completion.frequency == 7
-    assert habit2 == Habit(name=None,description=None) and habit2.completion.frequency == 1
+@pytest.mark.parametrize("habit",habit_list[0:2])
+def test_create_new_habit(habit):
+    habit1 = Analytics.create_new_habit(habit_name="Go for a walk",habit_description="Walking at least 1km a day.",frequency=7)
+    habit2 = Analytics.create_new_habit(habit_name="Clean the house",habit_description="Clean the house once a week.",frequency=1)
+    assert habit1 == habit and habit1.completion.frequency == 7
+    assert habit2 == habit and habit2.completion.frequency == 1
