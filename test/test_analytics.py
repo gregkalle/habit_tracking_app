7cf7b5dd3f25946525960ad26aea5bd505a0f69b
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-11-04 13:17:46 +0100

    add fixture at test_analytics

--- test/test_analytics.py
+++ test/test_analytics.py
@@ -5,28 +5,30 @@ from scr.analytics import Analytics
 from scr.habit import Habit
 from scr.sqlite_storage import SQLiteStorage
 
+@pytest.fixture
+def create_habits():
+    test_habit1=Habit(name="Go for a walk",description="Walking at least 1km a day.",habit_id=1,frequency=1)
+    test_habit2=Habit(name="Clean the house", description="Clean the house once a week.",habit_id=2,frequency=7)
+    test_habit3=Habit(name="Push ups", description="Excercise 10 push-ups every day.",habit_id=3,frequency=1)
+    test_habit4=Habit(name="Eat an apple", description="An apple a day keeps the doctor away.",habit_id=4,frequency=1)
+    test_habit5=Habit(name="Swimming", description="Swim 1km in the local swimming pool.",habit_id=5,frequency=7)
+    return [test_habit1,test_habit2,test_habit3,test_habit4,test_habit5]
+
 #set database to test database
 Habit.DEFAULT_STORAGE_STRATEGY = SQLiteStorage("test/test_data.db")
 
-test_habit1=Habit(name="Go for a walk",description="Walking at least 1km a day.",habit_id=1,frequency=1)
-test_habit2=Habit(name="Clean the house", description="Clean the house once a week.",habit_id=2,frequency=7)
-test_habit3=Habit(name="Push ups", description="Excercise 10 push-ups every day.",habit_id=3,frequency=1)
-test_habit4=Habit(name="Eat an apple", description="An apple a day keeps the doctor away.",habit_id=4,frequency=1)
-test_habit5=Habit(name="Swimming", description="Swim 1km in the local swimming pool.",habit_id=5,frequency=7)
-test_habit_list=[test_habit1,test_habit2,test_habit3,test_habit4,test_habit5]
 
-
-@pytest.mark.parametrize("habit_list",[test_habit_list])
-def test_load_habits(habit_list):
+def test_load_habits(create_habits):
     habits = Analytics.load_habits()
-    assert len(habits) == len(habit_list)
+    assert len(habits) == len(create_habits)
     for habit in habits:
-        assert habit in habit_list
+        assert habit in create_habits
     
 
-@pytest.mark.parametrize("habit, habit_id",[(habit,test_habit_list.index(habit)+1) for habit in test_habit_list])
-def test_get_current_tracked_habit(habit,habit_id):
-    currend_tracked_habit = Analytics.get_current_tracked_habit(habit_id=habit_id)
+@pytest.mark.parametrize("habit_id",[[1,2,3,4,5]])
+def test_get_current_tracked_habit(create_habits,habit_id):
+    for i,habit in enumerate(create_habits):
+        currend_tracked_habit = Analytics.get_current_tracked_habit(habit_id=habit_id[i])
     assert currend_tracked_habit == habit
 
 def test_get_current_tracked_habit_errors():
@@ -34,21 +36,20 @@ def test_get_current_tracked_habit_errors():
     with pytest.raises(ValueError,match=f"There is no habit with id {missing_id} in the database."):
         Analytics.get_current_tracked_habit(missing_id)
 
-@pytest.mark.parametrize("habit_list,frequency",[(test_habit_list,1),(test_habit_list,7)])
-def test_get_habits_with_frequency(habit_list,frequency):
-    habits_with_frequency=Analytics.get_habits_with_frequency(habit_list=habit_list,frequency=frequency)
+@pytest.mark.parametrize("frequency",[1,7])
+def test_get_habits_with_frequency(create_habits,frequency):
+    habits_with_frequency=Analytics.get_habits_with_frequency(habit_list=create_habits,frequency=frequency)
     for habit in habits_with_frequency:
         assert habit.completion.frequency == frequency
     #test if all habits with frequency is in habits with frequency.
-    habits_without_frequency = [habit for habit in habit_list if habit.completion.frequency!=frequency]
-    assert len(habits_without_frequency) == len(habit_list) - len(habits_with_frequency)
+    habits_without_frequency = [habit for habit in create_habits if habit.completion.frequency!=frequency]
+    assert len(habits_without_frequency) == len(create_habits) - len(habits_with_frequency)
 
-@pytest.mark.parametrize("habit_list,frequency",[(test_habit_list,7)])
-def test_get_habit_with_frequency_errors(habit_list,frequency):
+def test_get_habit_with_frequency_errors(create_habits):
     with pytest.raises(TypeError,match="Frequency not of type integer."):
-        Analytics.get_habits_with_frequency(habit_list=habit_list,frequency="seven")
+        Analytics.get_habits_with_frequency(habit_list=create_habits,frequency="seven")
     with pytest.raises(TypeError,match="Object not of type Habit."):
-        Analytics.get_habits_with_frequency(habit_list=[Habit(name=None,description=None),"Not a habit object."],frequency=frequency)
+        Analytics.get_habits_with_frequency(habit_list=[Habit(name=None,description=None),"Not a habit object."],frequency=7)
 
 @pytest.mark.parametrize("habit_id,result",[(1,28),(2,2),(3,14),(4,14),(5,1)])
 def test_get_longest_streak(habit_id,result):
@@ -86,25 +87,23 @@ def test_habit_to_dict_error():
     with pytest.raises(TypeError,match="Object not of type Habit."):
         Analytics.habit_to_dict("No habit object.")
 
-
-@pytest.mark.parametrize("habit_list",[test_habit_list])
-def test_get_habit_ids(habit_list):
-    id_list = Analytics.get_habit_ids(habit_list=habit_list)
-    for j,habit_id in enumerate(id_list):
-        assert j+1== habit_id
+@pytest.mark.parametrize("habit_id_list",[[1,2,3,4,5]])
+def test_get_habit_ids(create_habits,habit_id_list):
+    id_list = Analytics.get_habit_ids(habit_list=create_habits)
+    for i, habit_id in enumerate(id_list):
+        assert habit_id_list[i] == habit_id
 
 def test_get_habit_ids_error():
     with pytest.raises(TypeError,match="Object not of type Habit."):
         Analytics.get_habit_ids([Habit(name=None,description=None),"No habit object."])
 
-@pytest.mark.parametrize("habit",[test_habit_list[0:2]])
-def test_create_new_habit(habit):
+def test_create_new_habit(create_habits):
     habit1 = Analytics.create_new_habit(habit_name="Go for a walk",habit_description="Walking at least 1km a day.",frequency=7)
     habit2 = Analytics.create_new_habit(habit_name="Clean the house",habit_description="Clean the house once a week.",frequency=1)
     habit1.habit_id = 1
     habit2.habit_id = 2
-    assert habit1 == habit[0] and habit1.completion.frequency == 7
-    assert habit2 == habit[1] and habit2.completion.frequency == 1
+    assert habit1 == create_habits[0] and habit1.completion.frequency == 7
+    assert habit2 == create_habits[1] and habit2.completion.frequency == 1
 
 def test_create_new_habit_errors():
     with pytest.raises(TypeError, match="Habit name must be string."):
@@ -116,8 +115,8 @@ def test_create_new_habit_errors():
     with pytest.raises(ValueError, match="Frequency must be greater than 0."):
         Analytics.create_new_habit(habit_name="Go for a walk",habit_description="Walking at least 1km a day.",frequency=0)
 
-@pytest.mark.parametrize("habit_id, habit",[(1,test_habit1),(2,test_habit2),(3,test_habit3)])
-def test_change_habit_name_description(habit_id,habit):
+@pytest.mark.parametrize("habit_id",[1,2,3,4,5])
+def test_change_habit_name_description(habit_id):
     changed_habit = Analytics.change_habit_name_description(habit_id=habit_id,habit_name="New habit name.")
     assert changed_habit.name == "New habit name."
     changed_habit = Analytics.change_habit_name_description(habit_id=habit_id,habit_description="new description")
@@ -125,7 +124,7 @@ def test_change_habit_name_description(habit_id,habit):
     changed_habit = Analytics.change_habit_name_description(habit_id=habit_id,habit_name="new name", habit_description="new description")
     assert changed_habit.name == "new name" and changed_habit.description == "new description"
     changed_habit = Analytics.change_habit_name_description(habit_id=habit_id)
-    assert changed_habit == habit
+    assert changed_habit == Analytics.get_current_tracked_habit(habit_id=habit_id)
 
 def test_change_habit_name_description_errors():
     missing_id = 0
