b126d4e21a5ab26e47289ece66b61b3b62be2b48
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-11-04 12:53:14 +0100

    add fixture test_habit.py

--- test/test_habit.py
+++ test/test_habit.py
@@ -1,12 +1,14 @@
 import pytest
 from scr.habit import Habit
 
-test_habit1=Habit(name="Go for a walk",description="Walking at least 1km a day.",habit_id=1,frequency=1)
-test_habit2=Habit(name="Clean the house", description="Clean the house once a week.",habit_id=2,frequency=7)
-test_habit3=Habit(name="Push ups", description="Excercise 10 push-ups every day.",habit_id=3,frequency=1)
-test_habit4=Habit(name="Eat an apple", description="An apple a day keeps the doctor away.",habit_id=4,frequency=1)
-test_habit5=Habit(name="Swimming", description="Swim 1km in the local swimming pool.",habit_id=5,frequency=7)
-test_habit_list=[test_habit1,test_habit2,test_habit3,test_habit4,test_habit5]
+@pytest.fixture
+def create_habits():
+    test_habit1=Habit(name="Go for a walk",description="Walking at least 1km a day.",habit_id=1,frequency=1)
+    test_habit2=Habit(name="Clean the house", description="Clean the house once a week.",habit_id=2,frequency=7)
+    test_habit3=Habit(name="Push ups", description="Excercise 10 push-ups every day.",habit_id=3,frequency=1)
+    test_habit4=Habit(name="Eat an apple", description="An apple a day keeps the doctor away.",habit_id=4,frequency=1)
+    test_habit5=Habit(name="Swimming", description="Swim 1km in the local swimming pool.",habit_id=5,frequency=7)
+    return [test_habit1,test_habit2,test_habit3,test_habit4,test_habit5]
 
 def test__eq__():
     assert Habit(name="habit 1",description="habit 1") == Habit(name="habit 1",description="habit 1")
@@ -16,16 +18,16 @@ def test__eq__():
     assert Habit(name="habit 2",description="habit 2",habit_id=2) != Habit(name="habit 2",description="habit 2",habit_id=1)
 
 
-@pytest.mark.parametrize("habit, habit_id",[(test_habit1,1),(test_habit2,2),(test_habit3,3),(test_habit4,4),(test_habit5,5)])
-def test_load(habit, habit_id):
-    assert habit == Habit.load(habit_id=habit_id)
+@pytest.mark.parametrize("habit_id",[[1,2,3,4,5]])
+def test_load(create_habits, habit_id):
+    for i,habit in enumerate(create_habits):
+        assert habit == Habit.load(habit_id=habit_id[i])
 
-@pytest.mark.parametrize("habit_list",[test_habit_list])
-def test_load_all(habit_list):
+def test_load_all(create_habits):
     habits = Habit.load_all()
-    assert len(habits) == len(habit_list)
+    assert len(habits) == len(create_habits)
     for habit in habits:
-        assert habit in habit_list
+        assert habit in create_habits
 
 @pytest.mark.order(-2)
 def test_save_and_delete():
