2f730c10b3ff9414400cbc22a6487d3d6ff8a669
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-11-05 13:42:19 +0100

    run.py changed

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+from freezegun import freeze_time
 from main import main
 from scr.habit import Habit
 from scr.sqlite_storage import SQLiteStorage
 
 Habit.DEFAULT_STORAGE_STRATEGY = SQLiteStorage("test/test_data.db")
 
-main()
+@freeze_time("2024-10-27")
+def run():
+    main()
+
+run()
