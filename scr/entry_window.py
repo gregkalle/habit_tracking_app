eb3997f5f3db91edd81a28baf378ad3a0e4808e0
Gregor Makowski <gregor.j.makowski@gmail.com>
2024-11-11 13:07:16 +0100

    finelize entry window

--- scr/entry_window.py
+++ scr/entry_window.py
@@ -43,6 +43,8 @@ class EntryPopUp(PopUpWindow):
     def __init__(self, main_window, habit_id, behave = BEHAVE_NEW_HABIT):
         super().__init__(main_window=main_window,habit_id=habit_id)
 
+        #If behave is "new habit", a "new habit"-window is created,
+        #if behave is "change habit", a "change habit"-window is created.
         self.behave = behave
         if (self.behave == EntryPopUp.BEHAVE_NEW_HABIT and
                self.behave ==EntryPopUp.BEHAVE_CHANGE_HABIT):
@@ -51,10 +53,15 @@ class EntryPopUp(PopUpWindow):
 
         self.habit_frequency = tk.IntVar(self)
 
+        #pack habit name entry field
         self.entry_habit_name = self.pack_name_entry_field(label_text="habit name")
+        #pack habit description entry field
         self.entry_habit_description = self.pack_name_entry_field(label_text="habit description")
+
         if self.behave == EntryPopUp.BEHAVE_NEW_HABIT:
+            #pack  frequency entry field
             self.pack_frequency_entry_field(label_text="frequency",variable=self.habit_frequency)
+        #create and pack the buttons of the entry window
         self.pack_buttons()
 
 
@@ -63,16 +70,21 @@ class EntryPopUp(PopUpWindow):
         Pack label and entry field and return the entry field.
         
         Args:
-            label_text (str): The text for the label.
+            label_text (str): The text of the label.
         
         Returns:
             ttk.Entry: The entry field widget.
         """
         frame = ttk.Frame(self)
+        #create and pack the label to the frame
         ttk.Label(frame,anchor="w",text=label_text,padding=10).pack(side="left")
+        #create the entry field
         textbox = ttk.Entry(frame)
+        #pack the entry field to the frame
         textbox.pack(side="left")
+        #pack the frame to the entry window
         frame.pack()
+        #return entryfield
         return textbox
 
     def pack_frequency_entry_field(self, label_text, variable):
@@ -80,7 +92,7 @@ class EntryPopUp(PopUpWindow):
         Pack label and radio buttons for frequency selection.
         
         Args:
-            label_text (str): The text for the label.
+            label_text (str): The text of the label.
             variable (tk.IntVar): The variable to store the selected frequency.
 
         Raises:
@@ -89,6 +101,7 @@ class EntryPopUp(PopUpWindow):
         frame = ttk.Frame(self)
         ttk.Label(frame,anchor="w",text=label_text,padding=10).pack(side="left")
         try:
+            #create the radiobutton to select the frequency
             for text in self.main_window.USABLE_FREQUENCIES:
                 ttk.Radiobutton(frame,text=text.lower(),
                                 value=self.main_window.USABLE_FREQUENCIES[text],
@@ -126,19 +139,23 @@ class EntryPopUp(PopUpWindow):
             AttributeError: Main window has no attribute all_habits
             AttributeError: Main window has no attribute reload center frame
         """
+        #Habit must have a name, description or frequency
         if not (self.entry_habit_name.get() and self.entry_habit_description.get()\
                 and self.habit_frequency.get()):
+            #error message, if one property is empty.
             message = mb.Message(self,icon=mb.ERROR,type=mb.OK,title="INPUT ERROR",
                        message="""The name and description of your habit may not be
                         empty and you must select a frequency"""
                        )
             message.show()
         else:
+            #creat new habit and save it to the database
             habit = Habit(name=self.entry_habit_name.get(),
                          description=self.entry_habit_description.get(),
                          frequency=self.habit_frequency.get())
             habit.save()
             try:
+                #append the list all_habits of the main window
                 self.main_window.all_habits.append(habit)
             except AttributeError as exc:
                 self.destroy()
@@ -147,6 +164,7 @@ class EntryPopUp(PopUpWindow):
 
 
             try:
+                #reload the center_frame of the main window
                 self.main_window.reload_center_frame(self.main_window.all_habits)
             except AttributeError as exc:
                 self.destroy()
@@ -162,13 +180,16 @@ class EntryPopUp(PopUpWindow):
             AttributeError: Main window has no attribute center frame or selected habid id.
             AttributeError: Main window has no attribute load habits or all habits.
         """
+        #Habit must have a name or description
         if not (self.entry_habit_name.get() or self.entry_habit_description.get()):
+            #error message, if one property is empty.
             message = mb.Message(self,icon=mb.ERROR,type=mb.OK,title="INPUT ERROR",
                        message="The name and description of your habit may not be empty",
                        )
             message.show()
         else:
             try:
+                #change the name or the description of the habit
                 habit = Habit.change_habit_name_description\
                     (habit_id=self.habit_id,
                     habit_name=self.entry_habit_name.get(),
@@ -177,9 +198,12 @@ class EntryPopUp(PopUpWindow):
                 self.destroy()
                 raise AttributeError("""Main window has no attribute center frame
                                      or selected habid id.""") from exc
+            #save changes to database
             habit.save()
             try:
+                #reload all_habits
                 self.main_window.all_habits=Habit.load_all()
+                #reload the center_frame
                 self.main_window.reload_center_frame(self.main_window.all_habits)
             except AttributeError as exc:
                 self.destroy()
@@ -210,7 +234,7 @@ class DatePicker(PopUpWindow):
         Create and pack the buttons for the date picker window.
         """
         button_frame = ttk.Frame(self)
-        ttk.Button(button_frame, text="today", command=self.click_today,
+        ttk.Button(button_frame, text="today", command=self.mark_date,
                padding=10).pack(side="left")
         ttk.Button(button_frame, text="select date", command=self.click_date,
                padding=10).pack(side="left")
@@ -226,31 +250,12 @@ class DatePicker(PopUpWindow):
             AttributeError: Main window has no attribute selected habit id.
             AttributeError: Main window has no attribute load habits or all habits.
         """
-
-        #achtung noch überarbeiten!!!
-        try:
-            habit = ana.get_current_tracked_habit(
-                habit_id=self.habit_id,
-                habit_list=self.main_window.all_habits
-                )
-            habit.completion.mark_completed()
-        except AttributeError as exc:
-            self.destroy()
-            raise AttributeError("Main window has no attribute selected habit id."
-                                ) from exc
-        habit.save()
-        try:
-            self.main_window.all_habits = Habit.load_all()
-            self.main_window.reload_center_frame(self.main_window.all_habits)
-        except AttributeError as exc:
-            self.destroy()
-            raise AttributeError("Main window has no attribute load habits or all habits."
-                                ) from exc
-        self.destroy()
+        self.mark_date()
 
     def click_date(self):
         """
-        Mark the habit as completed for the selected date and update the main window.
+        Transforms the date input of the datepicker in datetime.date
+        format and call th mark_date method.
 
         Raises:
             AttributeError: Main window has no attribute selected habit id.
@@ -258,20 +263,38 @@ class DatePicker(PopUpWindow):
 
         """
         date_str = self.calendar.get_date()
+        #split and save the picked date in month, day and year
         month, day, year = [int(date_) for date_ in date_str.split("/")]
+        #add 2000 to year becaus it is given as YY
         year = 2000 + year
+        #create date as datetime day type and call mark_date method
+        self.mark_date(checked_date=date(year=year,month=month,day=day))
+
+    def mark_date(self,checked_date=date.today()):
+
+        """
+        Mark the habit as completed for the checked_date and update the main window.
+
+        Raises:
+            AttributeError: Main window has no attribute selected habit id.
+            AttributeError: Main window has no attribute load habits or all habits.
+        """
+        
         try:
             habit = ana.get_current_tracked_habit(
                 habit_id=self.habit_id,
                 habit_list=self.main_window.all_habits
                 )
-            habit.completion.mark_completed(checked_date=date(year=year,month=month,day=day))
+            #mark the habit as fulfilled the selected day
+            habit.completion.mark_completed(checked_date=checked_date)
         except AttributeError as exc:
             self.destroy()
             raise AttributeError("Main window has no attribute selected habit id."
                                 ) from exc
+        #save changes
         habit.save()
         try:
+            #reload all_habits and center frame of main window
             self.main_window.all_habits = Habit.load_all()
             self.main_window.reload_center_frame(self.main_window.all_habits)
         except AttributeError as exc:
@@ -333,7 +356,7 @@ class PopUpCalendar(PopUpWindow):
         Raises:
             TypeError: Completed dates is not iterable.
         """
-        #set beckgroundcolor for the creation day
+        #set beckgroundcolor for the creation day to red
         self.calendar.calevent_create(date=creation_date,text="Calendar",
                                       tags= "Creation date")
         self.calendar.tag_config("Creation date", background="red",
@@ -345,6 +368,7 @@ class PopUpCalendar(PopUpWindow):
                     if isinstance(completed,date):
                         if i==0:
                             if completed == creation_date:
+                                #set foreground color of the creation day to green if it is checked
                                 self.calendar.calevent_create(date=completed+timedelta(days=i),
                                                     text="Calendar",
                                                     tags= "Day one equal creation date")
